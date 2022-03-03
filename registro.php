b85a33e16a0664760364d8f85e36ad4eaec38118
MiguelRodriguez2000 <100252929+MiguelRodriguez2000@users.noreply.github.com>
2022-03-03 10:30:27 -0600

    màs avances

--- registro.php
+++ registro.php
@@ -5,9 +5,13 @@ if (isset($_POST["registrar"])) {
     }else{
         $usario = filter_var(strtolower($_POST["correo"]), FILTER_SANITIZE_EMAIL);
         $contra = (empty($_POST["contra"])) ? "1234": $_POST["contra"];
+        $nombre = htmlspecialchars($_POST["nombre"]);
+        $apellido = htmlspecialchars($_POST["apellido"]);
+        $rol = htmlspecialchars($_POST["rol"]);
 
         try {
-            $conexion = new PDO('mysql:host=localhost;dbname')
+            $conexion = new PDO('mysql:host=localhost;dbname=practicapw', 'root', '');
+            $insercion = $conexion->prepare('INSERT INTO usuarios')
         } catch (\Throwable $th) {
             //throw $th;
         }
