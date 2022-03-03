3f326e94b1be156b268944314ad0f908afef261f
MiguelRodriguez2000 <100252929+MiguelRodriguez2000@users.noreply.github.com>
2022-03-03 10:20:29 -0600

    màs avance

--- registro.php
+++ registro.php
@@ -2,7 +2,16 @@
 if (isset($_POST["registrar"])) {
     if (empty($POST["correo"])) {
         echo   "Favor de ingresar su correo electronico";
-    }else
+    }else{
+        $usario = filter_var(strtolower($_POST["correo"]), FILTER_SANITIZE_EMAIL);
+        $contra = (empty($_POST["contra"])) ? "1234": $_POST["contra"];
+
+        try {
+            $conexion = new PDO('mysql:host=localhost;dbname')
+        } catch (\Throwable $th) {
+            //throw $th;
+        }
+    }
 }
 
 
